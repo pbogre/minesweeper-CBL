f2c05809eebde9c0494dfdf63e0a676dd4300f8f
Azgorn <damian.vladimirov@gmail.com>
2023-10-10 23:02:38 +0200

    working fix for issue

--- MinesweeperTest.java
+++ MinesweeperTest.java
@@ -89,7 +89,6 @@ public class MinesweeperTest {
                 grid.add(cells[row][col]);
             }
         }
-        //createMines();
         frame.add(grid, BorderLayout.CENTER);
     }
 
